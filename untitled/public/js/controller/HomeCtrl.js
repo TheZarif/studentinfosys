1623386540ee20c96153200c0a635ab7a7bf8b2e
Zarif <Zarif@localhost>
2015-11-07 02:27:40 +0600

    Admin Dashboard added Create user panel

--- untitled/public/js/controller/HomeCtrl.js
+++ untitled/public/js/controller/HomeCtrl.js
@@ -2,7 +2,25 @@
  * Created by Zarif on 06/11/2015.
  */
 
-ctrls.controller('HomeCtrl', function ($scope, $rootScope) {
-    console.log($rootScope);
+ctrls.controller('HomeCtrl', function ($scope, $rootScope, $state, $http) {
+    if(!$rootScope.user) $state.go('login')
+
+    var baseUrl = $rootScope.baseUrl;
+
+    $scope.user = {}
+
+    $scope.setActive = function(active){
+        $scope.active = active;
+    }
+
+    $scope.createUser = function (newUser) {
+        $http.post(baseUrl + "users", newUser)
+            .success(function (data, status) {
+                console.log('User added');
+             })
+            .error(function (err) {
+
+            })
+    }
 
 })
