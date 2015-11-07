c6f8eaf5141ac6455b865892a716494bb278145d
Zarif <Zarif@localhost>
2015-11-07 23:43:31 +0600

    Angular-Toastr module added for Toast notifications

--- untitled/public/js/controller/HomeCtrl.js
+++ untitled/public/js/controller/HomeCtrl.js
@@ -2,7 +2,7 @@
  * Created by Zarif on 06/11/2015.
  */
 
-ctrls.controller('HomeCtrl', function ($scope, $rootScope, $state, $http) {
+ctrls.controller('HomeCtrl', function ($scope, $rootScope, $state, $http, toastr) {
     if(!$rootScope.user) $state.go('login')
 
     var baseUrl = $rootScope.baseUrl;
@@ -16,9 +16,21 @@ ctrls.controller('HomeCtrl', function ($scope, $rootScope, $state, $http) {
     $scope.createUser = function (newUser) {
         $http.post(baseUrl + "users", newUser)
             .success(function (data, status) {
+                toastr.success('User added!', 'Success!');
+                $scope.user = {};
                 console.log('User added');
-             })
-            .error(function (err) {
+            })
+            .error(function (res, status) {
+                var message = "";
+                if(status == 403){
+                    toastr.error('You are not logged in!', 'Error');
+                }
+                else if(status == 401) {
+                    toastr.error('You are not authorized to do that', 'Error');
+                }
+                else{
+                    toastr.error('Could not add user', 'Error');
+                }
 
             })
     }
