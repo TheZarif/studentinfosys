0ac6fd6ce8028b9172f80ae77538a36743567739
Zarif <Zarif@localhost>
2015-11-14 02:38:45 +0600

    Admin panel User CRUD complete

--- untitled/public/js/controller/UserCtrl.js
+++ untitled/public/js/controller/UserCtrl.js
@@ -7,23 +7,30 @@ ctrls.controller('UserCtrl', function ($scope, $rootScope, $state, $http, toastr
 
     var baseUrl = $rootScope.baseUrl;
     var userId = $rootScope.user.userId;
+    var defaultSelected = 'Students';
+
     $scope.roleType = $rootScope.user.roleType;
     $scope.authorizer = AuthorizationFactory.isAuthorized;
-
-    $scope.selectedItem = 'teacher';
-
-
+    $scope.selectedItem = defaultSelected;
+    $scope.semesters = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th'];
     $scope.user = {};
+
     var viewCreateUser = false;
     $scope.viewIfCreateUser = function () {
+        $scope.toggleViewCreateUser = function () {
+            if(viewCreateUser)      viewCreateUser = false;
+            viewCreateUser = true;
+            else {
+            }
+        };
         return viewCreateUser;
-    };
-    $scope.toggleViewCreateUser = function () {
-        if(viewCreateUser)      viewCreateUser = false;
-        else                    viewCreateUser = true;
     };
 
     $scope.createUser = function () {
+        if(!validate($scope.user)) {
+            toastr.error("Required fields missing", "Oops!");
+            return;
+        }
         $http.post(baseUrl + "users", $scope.user)
             .success(function (data, status) {
                 toastr.success('User added!', 'Success!');
@@ -32,16 +39,9 @@ ctrls.controller('UserCtrl', function ($scope, $rootScope, $state, $http, toastr
             })
             .error(function (res, status) {
                 var message = "";
-                if(status == 403){
-                    toastr.error('You are not logged in!', 'Error');
-                }
-                else if(status == 401) {
-                    toastr.error('You are not authorized to do that', 'Error');
-                }
-                else{
-                    toastr.error('Could not add user', 'Error');
-                }
-
+                if(status == 403){toastr.error('You are not logged in!', 'Error');}
+                else if(status == 401) {toastr.error('You are not authorized to do that', 'Error');}
+                else{toastr.error('Could not add user', 'Error');}
             })
     };
 
@@ -51,7 +51,7 @@ ctrls.controller('UserCtrl', function ($scope, $rootScope, $state, $http, toastr
     }
 
     $scope.getUsers = function(type){
-        $http.get(baseUrl + "users", type)
+        $http.get(baseUrl + "All" + type)
             .success(function (users) {
                 console.log('users retrieved for user: '+ userId);
                 $scope.users = users;
@@ -60,7 +60,6 @@ ctrls.controller('UserCtrl', function ($scope, $rootScope, $state, $http, toastr
                 if(status == 401) {
                     toastr.error('You are not authorized to do that', 'Error');
                     $scope.users = [];
-                    //$state.go();
                 }
                 else{
                     toastr.error('Could not load users', 'Error');
@@ -70,32 +69,45 @@ ctrls.controller('UserCtrl', function ($scope, $rootScope, $state, $http, toastr
     };
 
     $scope.editUser = function (user) {
-        $http.put(baseUrl + "users/" + user.userId, user)
+        $http.put(baseUrl + "users/" + user._id, user)
             .success(function (data) {
                 toastr.success("User updated");
             })
             .error(function (err) {
                 toastr.error("Error")
             })
+    };
+
+    $scope.deleteUser = function (user) {
+        var r = window.confirm("Are you sure?");
+        if(r == true)   {
+            $http.delete(baseUrl + "users/" + user._id)
+                .success(function (data) {
+                    toastr.success("User deleted");
+                    remove($scope.users, user)
+                })
+                .error(function (err) {
+                    toastr.error("Error")
+                })
+        }
+    };
+
+
+
+    $scope.ifStudent = function (user) {
+        return user.roleId == "Student";
     }
 
-    $scope.users = [
-        {
-            _id: "10101",
-            userName: "Sami",
-            email: "sami@yahoo.com",
-            contactNo: "+88017171756",
-            designation: ""
-        },
-        {
-            _id: "10111",
-            userName: "Jamil",
-            email: "jamil@yahoo.com",
-            contactNo: "+88017171756",
-            designation: "Associate Professor"
-        }
-    ]
+    $scope.getUsers(defaultSelected);
 
-    $scope.getUsers();
+    function remove(objects, object){
+        var index = objects.indexOf(object);
+        if(index > -1)      objects.splice(index, 1);
+    }
+
+    function validate(user){
+        if(!user)   return false;
+        else return !!user.userName && !!user.email && !!user.password && !!user.roleId;
+    }
 
 });
